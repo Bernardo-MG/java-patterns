1927d113e78286e93ced8071409dca41ff48827c
Bernardo Martínez Garrido <programming@wandrell.com>
2015-02-14 23:55:47 +0100

    Changes to Travis config.

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-# This script deploys the application using the POM cofiguration
+# This script deploys the application using the POM configuration
 # It is triggered only commits to the master or develop branches. Pulls are ignored
 
-echo "Branch " + $TRAVIS_BRANCH
+echo "Version " + $TRAVIS_JAVA_VERSION
+echo "Version " + $TRAVIS_JDK_VERSION
 
 if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [[ "$TRAVIS_BRANCH" == "master" || "$TRAVIS_BRANCH" == "develop" ]]; then
 
