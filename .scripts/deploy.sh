6acf1f7a8704ee9b6bfa9046b7deb26671115cd2
Bernardo Martínez Garrido <programming@wandrell.com>
2015-02-14 23:51:17 +0100

    Modified Travis config.

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -2,8 +2,16 @@
 # This script deploys the application using the POM cofiguration
 # It is triggered only commits to the master or develop branches. Pulls are ignored
 
+echo "Branch " + $TRAVIS_BRANCH
+
 if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [[ "$TRAVIS_BRANCH" == "master" || "$TRAVIS_BRANCH" == "develop" ]]; then
 
+   echo "Deploying Maven site"
+
    mvn deploy --settings ~/settings.xml
+
+else
+
+   echo "Maven site won't be deployed"
 
 fi
