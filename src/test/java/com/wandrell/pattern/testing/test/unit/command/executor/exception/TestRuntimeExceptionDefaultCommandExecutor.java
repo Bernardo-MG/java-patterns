7b6282e050b99f2eda29d32ca4698e8fe377b67a
bernardo.martinez <bernardo.martinez>
2015-12-03 09:23:46 +0100

    Style corrections.

--- src/test/java/com/wandrell/pattern/testing/test/unit/command/executor/exception/TestRuntimeExceptionDefaultCommandExecutor.java
+++ src/test/java/com/wandrell/pattern/testing/test/unit/command/executor/exception/TestRuntimeExceptionDefaultCommandExecutor.java
@@ -21,6 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+
 package com.wandrell.pattern.testing.test.unit.command.executor.exception;
 
 import org.mockito.Mockito;
