c4fe1f0024d54068db39d091706e10d3aea757a8
Bernardo Martínez Garrido <programming@wandrell.com>
2015-12-05 18:21:38 +0100

    Sorted test context files.

--- src/test/java/com/wandrell/pattern/testing/util/conf/TestContextConfig.java
+++ src/test/java/com/wandrell/pattern/testing/util/conf/TestContextConfig.java
@@ -10,20 +10,20 @@ package com.wandrell.pattern.testing.util.conf;
  */
 public final class TestContextConfig {
 
-    public static final String OUTPUTTER_XML_DTD_VALIDATION                = "classpath:context/test-outputter-xml-dtd-validation.xml";
-    public static final String OUTPUTTER_XML_NO_VALIDATION                 = "classpath:context/test-outputter-xml-no-validation.xml";
-    public static final String OUTPUTTER_XML_XSD_VALIDATION                = "classpath:context/test-outputter-xml-xsd-validation.xml";
-    public static final String PARSER_XML_DTD_VALIDATION_EXCEPTION         = "classpath:context/test-parser-xml-dtd-exception.xml";
-    public static final String PARSER_XML_DTD_VALIDATION_NO_VALIDATES      = "classpath:context/test-parser-xml-dtd-no-validates.xml";
-    public static final String PARSER_XML_FILTERED_DTD_VALIDATION          = "classpath:context/test-parser-xml-dtd-filtered.xml";
-    public static final String PARSER_XML_FILTERED_NO_VALIDATION           = "classpath:context/test-parser-xml-no-validation-filtered.xml";
-    public static final String PARSER_XML_FILTERED_NO_VALIDATION_FULL      = "classpath:context/test-parser-xml-no-validation-filtered-full.xml";
-    public static final String PARSER_XML_FILTERED_XSD_VALIDATION          = "classpath:context/test-parser-xml-xsd-filtered.xml";
-    public static final String PARSER_XML_FILTERED_XSD_VALIDATION_DEFAULTS = "classpath:context/test-parser-xml-xsd-filtered-defaults.xml";
-    public static final String PARSER_XML_NO_VALIDATION_EXCEPTION          = "classpath:context/test-parser-xml-no-validation-exception.xml";
-    public static final String PARSER_XML_NOT_VALIDATED_EXCEPTION          = "classpath:context/test-parser-xml-not-validated-exception.xml";
-    public static final String PARSER_XML_XSD_VALIDATION_EXCEPTION         = "classpath:context/test-parser-xml-xsd-exception.xml";
-    public static final String PARSER_XML_XSD_VALIDATION_NO_VALIDATES      = "classpath:context/test-parser-xml-xsd-no-validates.xml";
+    public static final String OUTPUTTER_XML_DTD_VALIDATION                = "classpath:context/outputter/test-outputter-xml-dtd-validation.xml";
+    public static final String OUTPUTTER_XML_NO_VALIDATION                 = "classpath:context/outputter/test-outputter-xml-no-validation.xml";
+    public static final String OUTPUTTER_XML_XSD_VALIDATION                = "classpath:context/outputter/test-outputter-xml-xsd-validation.xml";
+    public static final String PARSER_XML_DTD_VALIDATION_EXCEPTION         = "classpath:context/parser/xml/test-parser-xml-dtd-exception.xml";
+    public static final String PARSER_XML_DTD_VALIDATION_NO_VALIDATES      = "classpath:context/parser/xml/test-parser-xml-dtd-no-validates.xml";
+    public static final String PARSER_XML_FILTERED_DTD_VALIDATION          = "classpath:context/parser/xml/filtered/test-parser-xml-dtd-filtered.xml";
+    public static final String PARSER_XML_FILTERED_NO_VALIDATION           = "classpath:context/parser/xml/filtered/test-parser-xml-no-validation-filtered.xml";
+    public static final String PARSER_XML_FILTERED_NO_VALIDATION_FULL      = "classpath:context/parser/xml/filtered/test-parser-xml-no-validation-filtered-full.xml";
+    public static final String PARSER_XML_FILTERED_XSD_VALIDATION          = "classpath:context/parser/xml/filtered/test-parser-xml-xsd-filtered.xml";
+    public static final String PARSER_XML_FILTERED_XSD_VALIDATION_DEFAULTS = "classpath:context/parser/xml/filtered/test-parser-xml-xsd-filtered-defaults.xml";
+    public static final String PARSER_XML_NO_VALIDATION_EXCEPTION          = "classpath:context/parser/xml/test-parser-xml-no-validation-exception.xml";
+    public static final String PARSER_XML_NOT_VALIDATED_EXCEPTION          = "classpath:context/parser/xml/test-parser-xml-not-validated-exception.xml";
+    public static final String PARSER_XML_XSD_VALIDATION_EXCEPTION         = "classpath:context/parser/xml/test-parser-xml-xsd-exception.xml";
+    public static final String PARSER_XML_XSD_VALIDATION_NO_VALIDATES      = "classpath:context/parser/xml/test-parser-xml-xsd-no-validates.xml";
     public static final String XML                                         = "classpath:context/test-xml.xml";
 
     /**
